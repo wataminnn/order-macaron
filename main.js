7648502a16942ec883312519c083fe7e54f1dceb
watami <watamin.m@gmail.com>
2023-09-30 02:22:13 +0900

    動き追加　9/30 2:22

--- main.js
+++ main.js
@@ -1,1 +1,50 @@
 
+$(window).scroll(function () {
+    var scrollAnimationElm = document.querySelectorAll('.scroll_up , .scroll_left , .scroll_right');
+    var scrollAnimationFunc = function () {
+      for (var i = 0; i < scrollAnimationElm.length; i++) {
+        var triggerMargin = 150;
+        if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+          scrollAnimationElm[i].classList.add('on');
+        }
+      }
+    }
+    window.addEventListener('load', scrollAnimationFunc);
+    window.addEventListener('scroll', scrollAnimationFunc);
+  });
+
+  $(function(){
+
+    $(window).on('load',function(){
+
+      $('.loader').delay(600).fadeOut(600);
+
+      $('.loader-bg').delay(900).fadeOut(800);
+  });
+  
+    setTimeout(function(){
+      $('.loader-bg').fadeOut(600);
+    },5000);
+  });
+
+
+function SmoothTextAnime() {
+	$('.smoothTextTrigger').each(function(){ 
+		var elemPos = $(this).offset().top-50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('smoothTextAppear');
+		}else{
+		$(this).removeClass('smoothTextAppear');
+		}
+		});	
+}
+
+
+$(window).scroll(function () {
+	SmoothTextAnime();
+});
+$(window).on('load', function () {
+	SmoothTextAnime();
+});
